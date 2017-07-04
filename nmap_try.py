c5e6f5734cc00c9415c166ddbf3a6418ef8a6e5d
alexander <alexander.ananiev@gmail.com>
2017-07-04 14:05:20 +0300

    02

--- nmap_try.py
+++ nmap_try.py
@@ -1,34 +1,32 @@
 import sys
-import os
-
 import nmap
 
 
-# print(nmap.__version__)
-# try:
-#     nm = nmap.PortScanner()  # instantiate nmap.PortScanner object
-# except nmap.PortScannerError:
-#     print('Nmap not found', sys.exc_info()[0])
-#     sys.exit(0)
-# except:
-#     print("Unexpected error:", sys.exc_info()[0])
+print("python-nmap version -", nmap.__version__)
+try:
+    nm = nmap.PortScanner()  # instantiate nmap.PortScanner object
+except nmap.PortScannerError:
+    print('Nmap not found', sys.exc_info()[0])
+    sys.exit(0)
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
+else:
+    print("Ok...")
 
-
-nm = nmap.PortScanner()  # instantiate nmap.PortScanner object
-nm.scan('127.0.0.1', '22-443')  # scan host 127.0.0.1, ports from 22 to 443
+nm.scan('127.0.0.1', '01-1000')  # scan host 127.0.0.1, ports from 01 to 1000
 nm.command_line()  # get command line used for the scan : nmap -oX - -p 22-443 127.0.0.1
 nm.scaninfo()  # get nmap scan information {'tcp': {'services': '22-443', 'method': 'connect'}}
 nm.all_hosts()   # get all hosts that were scanned
 for host in nm.all_hosts():
-    print('----------------------------------------------------')
+    print('----------------------------------')
     print('Host : %s (%s)' % (host, nm[host].hostname()))
     print('State : %s' % nm[host].state())
 
     for proto in nm[host].all_protocols():
-        print('----------')
+        print('----------------------------------')
         print('Protocol : %s' % proto)
 
-        lport = nm[host][proto].keys()
-        lport.sort()
+        lport = sorted(nm[host][proto])
         for port in lport:
             print('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
